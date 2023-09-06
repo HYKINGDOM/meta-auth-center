a18afd5fc4f58102e30632da22fd2913c49669e9
yihurCoder <1370054591@qq.com>
2023-09-06 14:58:14 +0800

    Initial commit

---
+++ src/main/java/com/java/meta/auth/MetaAuthCenterApplication.java
@@ -0,0 +1,16 @@
+package com.java.meta.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hy
+ */
+@SpringBootApplication
+public class MetaAuthCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaAuthCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/meta/auth/MetaAuthCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.meta.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaAuthCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
